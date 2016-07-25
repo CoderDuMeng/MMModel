c75864d9308db813e8cba6b7458f6b3fd093ca58
DemoDu <dom@detu.com>
2016-07-25 13:12:38 +0800

    Initial Commit

---
+++ MMModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMModel
+//
+//  Created by detu on 16/7/25.
+//  Copyright © 2016年 demoDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMModel
+//
+//  Created by detu on 16/7/25.
+//  Copyright © 2016年 demoDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
